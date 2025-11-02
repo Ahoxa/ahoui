61ef4ff22717db8d5e9f6046995fe39981b69b8c
ahoxa <yujinyuto1223@gmail.com>
2025-11-02 11:36:50 +0900

    feat: add tsup.config

---
+++ packages/react/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.tsx"],
+  format: ["esm", "cjs"],
+  dts: true,
+  clean: true,
+  external: ["react", "react-dom"],
+  treeshake: true,
+  esbuildOptions(options) {
+    options.jsx = "automatic";
+  },
+});
